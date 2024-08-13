e929d0ebcd0604153a9550bbafec11857367e1df
kimgusxo <gusxoas9906@naver.com>
2024-08-13 18:04:50 +0900

    reply update

--- src/main/java/com/example/noticeboard/domain/Reply.java
+++ src/main/java/com/example/noticeboard/domain/Reply.java
@@ -2,7 +2,9 @@ package com.example.noticeboard.domain;
 
 import lombok.Builder;
 import lombok.Data;
+import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.annotation.Transient;
 import org.springframework.data.relational.core.mapping.Table;
 
@@ -17,7 +19,11 @@ public class Reply {
 
     private String writer;
     private String content;
+
+    @CreatedDate
     private LocalDate registrationDate;
+
+    @LastModifiedDate
     private LocalDate updateDate;
 
     private Long boardId;
